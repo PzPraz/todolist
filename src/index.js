ec5c3e3ebe27535db873d2c5d22880b6cc14dfa0
pzpraz <95980337+PzPraz@users.noreply.github.com>
2025-01-25 22:53:31 +0700

    implemented skeleton for the UI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import './reset.css'
 import './styles.css'
 
 import { Task } from "./functions/Task.js";
+import {select} from './functions/domHelpers.js'
+
 
 //only for debugging purposes
 window.Task = Task
+
+//
+
+const projectForm = select('#add-project-form')
+const addProjectBtn = select('.add-project')
+
+addProjectBtn.addEventListener('click', () => {
+    addProjectBtn.classList.add('hide')
+    projectForm.classList.add('show')
+    projectForm.classList.remove('hide')
+})
+
